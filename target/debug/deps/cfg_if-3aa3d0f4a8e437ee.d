303d5190bd84c8aec3210756bc3eea5466f6f04a
Filip Bušek <filip.busek@protonmail.com>
2023-12-23 19:39:46 +0100

    Úprava help stringu

--- target/debug/deps/cfg_if-3aa3d0f4a8e437ee.d
+++
@@ -1,5 +0,0 @@
-/home/filip/Dokumenty/rust/rust-webserver/target/debug/deps/libcfg_if-3aa3d0f4a8e437ee.rmeta: /home/filip/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/filip/Dokumenty/rust/rust-webserver/target/debug/deps/cfg_if-3aa3d0f4a8e437ee.d: /home/filip/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/filip/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- target/debug/deps/rust_webserver-b06a991968dc00c1.d
+++
@@ -1,7 +0,0 @@
-/home/filip/Dokumenty/rust/rust-webserver/target/debug/deps/librust_webserver-b06a991968dc00c1.rmeta: src/main.rs src/workers.rs src/config.rs
-
-/home/filip/Dokumenty/rust/rust-webserver/target/debug/deps/rust_webserver-b06a991968dc00c1.d: src/main.rs src/workers.rs src/config.rs
-
-src/main.rs:
-src/workers.rs:
-src/config.rs:
